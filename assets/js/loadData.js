f7155160cd5b6d90635614146653e4ad56b82ba6
n529joker <ngengerawlings004@gmail.com>
2023-09-14 22:15:59 +0100

    updating template

--- assets/js/loadData.js
+++ assets/js/loadData.js
@@ -5,42 +5,49 @@ const categories_div = document.getElementById("categories-div");
 const pages = document.getElementById("pages");
 const pages_div = document.getElementById("pages-div");
 
-const loadData = async (page = 0, q = "") => {
-  let URL =
-    "https://pink-famous-eel.cyclic.app/data/BDrw56tuSiCp7lmg5K5NA69RBEAKgA08/";
-  let data, res;
-  if (q !== "") {
-    URL = `${URL}?q=${q}`;
-    res = await fetch(URL);
-    data = await res.json();
-    if (!data.Error) {
-      type.innerHTML = `for '${q}'`;
-      displayCards(data.data);
-    } else {
-      cards.innerHTML = `Results for '${q}' not found. Try another search term.`;
-      categories_div.style.display = "none";
-      pages_div.style.display = "none";
-    }
-  } else {
-    if (page >= 1) {
-      URL = `${URL}?page=${page}`;
-    } else {
-      URL = `${URL}?page=4`;
-    }
-    res = await fetch(URL);
-    data = await res.json();
-    displayCards(data.data);
-    displayPagination(data.pages);
-    displayCategories(data.data);
-  }
+const loadDataParam = async (param) => {
+  let URL = `https://pink-famous-eel.cyclic.app/data/BDrw56tuSiCp7lmg5K5NA69RBEAKgA08/${param}`;
+  let res = await fetch(URL);
+  let data = await res.json();
+  type.innerHTML = `for ${param}`;
+  getTags(data.data, "tag");
+  displayCards(data.data);
+  pages_div.innerHTML = "";
+  categories_div.innerHTML = "";
 };
 
 const handleQuery = (e) => {
   e.preventDefault();
   let q = e.target.query.value;
-  //check for all instances of whitespace, replace with +
   q = q.replace(/\s+/g, "+");
-  loadData(q);
+  loadDataQuery(q);
+};
+
+const loadData = async (Q, page = 0) => {
+  page = page === 0 ? Math.round(Math.random() * 10) : page;
+  let URL = `https://pink-famous-eel.cyclic.app/data/BDrw56tuSiCp7lmg5K5NA69RBEAKgA08/?page=${page}`;
+  let res = await fetch(URL);
+  let data = await res.json();
+  if (data.Error) {
+    cards.innerHTML = data.Error;
+  } else {
+    displayCards(data.data);
+    getTags(data.data, "tag");
+    displayPagination(data.pages);
+  }
+};
+
+const loadDataQuery = async (Q) => {
+  let URL = `https://pink-famous-eel.cyclic.app/data/BDrw56tuSiCp7lmg5K5NA69RBEAKgA08/?q=${Q}`;
+  let res = await fetch(URL);
+  let data = await res.json();
+  categories_div.innerHTML = "";
+  pages_div.innerHTML = "";
+  if (data.Error) {
+    cards.innerHTML = data.Error;
+  } else {
+    displayCards(data.data);
+  }
 };
 
 const displayCards = (data) => {
@@ -49,95 +56,103 @@ const displayCards = (data) => {
     let card = document.createElement("div");
     card.classList.add("card", "info-card", "customers-card");
     card.innerHTML = `
-    <div class="card-body">
-        <h5 class="card-title text-center">${item.title}</h5>
-            <hr/>
-            <div class="d-flex align-items-center justify-content-center flex-lg-row flex-sm-row">
-                <img class="icon rounded" width="100" src="${item.imgSrc}" onerror="this.src='assets/img/logo2.png'" alt="LOGO" />
-                </div>
-
-                <div class="ps-3">
-                <div>
-                  <p class="text-left small pt-1 fw-bold">Description</p>
-                  <p class="text small">${item.description}</p>
-                </div>
-                <div>
-                <p class="text-left small pt-1 fw-bold">Tags</p>
-                <p class="text small">${item.tag}</p>
-                </div>
-                  <a type="button" class="btn btn-outline-secondary" href="${item.link}" onerror="this.href='#'">visit site</a>
-                </div>
-            </div>
-        </div>
-    `;
+      <div class="card-body">
+          <h5 class="card-title text-center">${item.title}</h5>
+              <hr/>
+              <div class="d-flex align-items-center justify-content-center flex-lg-row flex-sm-row">
+                  <img class="icon rounded" width="100" src="${item.imgSrc}" onerror="this.src='assets/img/logo.png'"/>
+                  </div>
+  
+                  <div class="ps-3">
+                  <div>
+                    <p class="text-left small pt-1 fw-bold">Description</p>
+                    <p class="text small">${item.description}</p>
+                  </div>
+                  <div>
+                  <p class="text-left small pt-1 fw-bold">Tags</p>
+                  <p class="text small">${item.tag}</p>
+                  </div>
+                    <a type="button" class="btn btn-outline-secondary" href="${item.link}" onerror="this.href='#'">visit site</a>
+                  </div>
+              </div>
+          </div>
+      `;
     cards.appendChild(card);
   }
 };
 
 const displayPagination = (data) => {
   pages.innerHTML = "";
-  let li = document.createElement("li");
-  li.classList.add("page-item");
-  li.innerHTML = `<a class="page-link" href="#" aria-label="Previous">
-  <span aria-hidden="true">&laquo;</span>
-</a>`;
-  pages.appendChild(li);
+
+  const previousLink = `
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+  `;
+  pages.insertAdjacentHTML("beforeend", previousLink);
+
   for (let i = 1; i <= data; i++) {
-    let li = document.createElement("li");
-    li.classList.add("page-item");
-    li.innerHTML = `<a class="page-link" href="#" onclick="loadData(${i})">${i}</a>`;
-    pages.appendChild(li);
+    const pageLink = `
+      <li class="page-item">
+        <a class="page-link" href="#" onclick="loadData(${i})">${i}</a>
+      </li>
+    `;
+    pages.insertAdjacentHTML("beforeend", pageLink);
   }
-  let li2 = document.createElement("li");
-  li2.classList.add("page-item");
-  li2.innerHTML = `<a class="page-link" href="#" aria-label="Next">
-  <span aria-hidden="true">&raquo;</span>
-</a>`;
-  pages.appendChild(li2);
+
+  const nextLink = `
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  `;
+  pages.insertAdjacentHTML("beforeend", nextLink);
 };
 
-const displayCategories = (data) => {
-  categories.innerHTML = "";
-  for (let item of data) {
-    if (item.tag) {
-      if (item.tag.includes(",")) {
-        let tags = item.tag.split(",");
-        for (let tag of tags) {
-          let button = document.createElement("a");
-          button.innerHTML = tag;
-          button.href = "#";
-          button.classList.add("btn", "btn-outline-secondary", "m-1");
-          button.onclick = () => loadDataParam(tag.trim());
-          categories.appendChild(button);
+const getTags = (data, tag) => {
+  const tags = [];
+  let data_ = data.map((item) => item[tag]);
+  for (let tag of data_) {
+    if (tag !== null) {
+      if (tag.includes(",")) {
+        let itags = tag.split(",");
+        for (let itag of itags) {
+          tags.push(itag.trim());
         }
       } else {
-        let button = document.createElement("a");
-        button.classList.add("btn", "btn-outline-secondary", "m-1");
-        button.innerHTML = item.tag;
-        button.href = "#";
-        button.onclick = () => loadDataParam(item.tag.trim());
-        categories.appendChild(button);
+        tags.push(tag.trim());
       }
     }
   }
+  displayCategories(tags);
 };
 
-const loadDataParam = async (param) => {
-  let url = `https://pink-famous-eel.cyclic.app/data/ph46q0ARS3etpxpKnHRGIBPLEO0Ffqbj/${param}`;
-  let res = await fetch(url);
-  let data = await res.json();
-  displayCards(data.data);
+const displayCategories = (tags) => {
+  categories.innerHTML = "";
+  for (let tag of tags) {
+    let button = document.createElement("a");
+    button.innerHTML = tag;
+    button.href = "#";
+    button.setAttribute("id", tag);
+    button.classList.add("btn", "btn-outline-secondary", "m-1");
+    button.onclick = () => loadDataParam(tag);
+    categories.appendChild(button);
+  }
 };
 
 const changeIcon = (e) => {
-  //change the bi-search icon to bi-x when clicked and vice versa
-  const icon = document.getElementById("search-icon");
-  if (icon.classList.contains("bi-search")) {
-    icon.classList.remove("bi-search");
-    icon.classList.add("bi-x");
+  let elClasses = e.target.classList;
+  let elId = e.target.getAttribute("id");
+  if (elClasses.contains("bi-x")) {
+    let new_class = elId === "bars" ? "bi-bars" : "bi-search";
+    elClasses.remove("bi-x");
+    elClasses.add(new_class);
   } else {
-    icon.classList.remove("bi-x");
-    icon.classList.add("bi-search");
+    elClasses.remove(elId === "bars" ? "bi-bars" : "bi-search");
+    elClasses.add("bi-x");
   }
 };
 
